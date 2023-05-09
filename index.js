79fdacf70f9fcb97d926a072f5936ee4ccc8ca9a
emartinez <enri.fc.98@gmail.com>
2023-05-09 10:18:34 +0000

    use linter to enforce the same coding style

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(
-	bodyParser.urlencoded({
-		extended: true,
-	})
-)
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+
 app.get('/', (request, response) => {
-	response.json({
-		info: "Express API running!"
-	})
-})
+  response.json({
+    info: 'Express API running!',
+  });
+});
+
 app.listen(port, () => {
-	console.log("app runing on port %d", port)
-})
+  console.log('app runing on port %d', port);
+});
